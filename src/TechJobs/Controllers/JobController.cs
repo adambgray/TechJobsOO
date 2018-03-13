eb315a9bc1fe4ffde4243a057542756fc425a1c1
Adam Gray <adambgray@gmail.com>
2018-03-13 16:47:25 -0400

    Refactored ViewModels, implemented add new job functionality

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -20,7 +20,9 @@ namespace TechJobs.Controllers
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
 
-            return View();
+            Models.Job someJob = jobData.Find(id);            
+            
+            return View(someJob);
         }
 
         public IActionResult New()
@@ -35,6 +37,25 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (newJobViewModel.Name != null)
+            {
+                Models.Job newJob = new Models.Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+                };
+                int newJobid = newJob.ID;
+                jobData.Jobs.Add(newJob);
+                //return View("Index", newJob);
+                return RedirectToAction("", new { id = newJobid });
+                
+                
+                
+            };
+
 
             return View(newJobViewModel);
         }
---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        public List<JobFieldType> Columns { get; set; }
+
+        public JobFieldType Column { get; set; } = JobFieldType.All;
+
+    }
+}
